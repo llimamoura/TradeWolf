99840d6bb4284db486af9330881f06d49a35605d
Gabriel Lima <gabriellglma9@gmail.com>
2025-05-02 17:00:30 -0300

    add SingIn with google

---
+++ app/src/main/java/com/example/tradewolfapp/viewModel/LoginWithGoogleViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.example.tradewolfapp.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.tradewolfapp.repository.AuthFirebaseRepository
+
+class LoginWithGoogleViewModelFactory (
+    private val authRepository: AuthFirebaseRepository
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return LoginWithGoogleViewModel(authRepository) as T
+
+
+    }
+}
--- app/src/main/java/com/example/tradewolfapp/views/auth/LoginScreen.kt
+++ app/src/main/java/com/example/tradewolfapp/views/auth/LoginScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -35,8 +36,11 @@ import com.example.tradewolfapp.model.AuthModel
 import com.example.tradewolfapp.repository.AuthFirebaseRepository
 import com.example.tradewolfapp.ui.theme.DarkGray
 import com.example.tradewolfapp.ui.theme.ForgotColor
+import com.example.tradewolfapp.viewModel.LoginResult
 import com.example.tradewolfapp.viewModel.LoginState
 import com.example.tradewolfapp.viewModel.LoginViewModel
+import com.example.tradewolfapp.viewModel.LoginWithGoogleViewModel
+import com.example.tradewolfapp.viewModel.LoginWithGoogleViewModelFactory
 import com.example.tradewolfapp.views.components.IconButtonComponent
 import com.example.tradewolfapp.views.components.MainButtonComponent
 import com.example.tradewolfapp.views.components.TextDivider
@@ -45,12 +49,16 @@ import com.google.firebase.auth.FirebaseUser
 @Composable
 fun LoginScreen(
     navController: NavController,
+    loginWithGoogleViewModel: LoginWithGoogleViewModel = viewModel(
+        factory = LoginWithGoogleViewModelFactory(AuthFirebaseRepository())
+    ),
     viewModel: LoginViewModel = viewModel {
         LoginViewModel(AuthFirebaseRepository())
     },
-    onLoginSuccess: (FirebaseUser) -> Unit 
+    onLoginSuccess: (FirebaseUser) -> Unit
 ) {
     val loginState by viewModel.loginState.collectAsState()
+    val loginGoogleState by viewModel.loginState.collectAsState()
 
     Column(
         modifier = Modifier
@@ -61,7 +69,7 @@ fun LoginScreen(
 
     ) {
         when (val state = loginState) {
-            is LoginState.Idle -> LoginForm(viewModel::login)
+            is LoginState.Idle -> LoginForm(viewModel::login, loginWithGoogleViewModel = loginWithGoogleViewModel)
             is LoginState.Loading -> {
                 Column (
                     modifier = Modifier.fillMaxSize(),
@@ -89,9 +97,10 @@ fun LoginScreen(
 
 
 @Composable
-fun LoginForm (onLogin: (AuthModel) -> Unit) {
+fun LoginForm (onLogin: (AuthModel) -> Unit, loginWithGoogleViewModel: LoginWithGoogleViewModel,) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    val loginWithGoogleState by loginWithGoogleViewModel.loginState.collectAsState()
     
     Column {
         Text(
@@ -157,7 +166,19 @@ fun LoginForm (onLogin: (AuthModel) -> Unit) {
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceAround
         ) {
-            IconButtonComponent(icon = R.drawable.google_logo)
+            when (loginWithGoogleState) {
+                is LoginResult.Idle -> {
+                    val context = LocalContext.current
+                    Button(onClick = { loginWithGoogleViewModel.loginWithGoogle(context = context) }) {
+                        IconButtonComponent(icon = R.drawable.google_logo)
+                    }
+                }
+                is  LoginResult.Loading ->  CircularProgressIndicator(color = Color.White)
+                is LoginResult.Success -> Text(text = "sucesso")
+                is LoginResult.Failure -> Text(text = "erro")
+            }
+
+
         }
 
         Spacer(modifier = Modifier.height(65.dp))
