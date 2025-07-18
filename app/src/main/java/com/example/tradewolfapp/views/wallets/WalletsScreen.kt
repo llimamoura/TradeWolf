f9663e4edd9e6a673cac868398c6bffbfb996de1
Gabriel lima de moura <81448870+llimamoura@users.noreply.github.com>
2025-07-18 17:53:39 -0300

    Revert "Feature nav bar"

--- app/src/main/java/com/example/tradewolfapp/views/wallets/WalletsScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.tradewolfapp.views.wallets
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun WalletsScreen() {
-    Text(text = "Wallets")
-}
