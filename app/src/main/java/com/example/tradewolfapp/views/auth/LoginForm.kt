b8df80f9f518f9c4e160b57d6fb74010a099c507
Felipe silva <lipesx07@gmail.com>
2025-08-12 01:41:13 -0300

    refatorando LoginForm

--- app/src/main/java/com/example/tradewolfapp/views/auth/LoginForm.kt
+++ app/src/main/java/com/example/tradewolfapp/views/auth/LoginForm.kt
@@ -3,12 +3,15 @@ package com.example.tradewolfapp.views.auth
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,18 +21,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tradewolfapp.R
 import com.example.tradewolfapp.model.AuthModel
 import com.example.tradewolfapp.repository.AuthFirebaseRepository
+import com.example.tradewolfapp.ui.theme.BlueLogo
 import com.example.tradewolfapp.ui.theme.ForgotColor
 import com.example.tradewolfapp.viewModel.auth.LoginWithGoogleViewModel
 import com.example.tradewolfapp.viewModel.auth.LoginWithGoogleViewModelFactory
@@ -49,6 +55,7 @@ fun LoginForm(
 ) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var rememberMe by remember { mutableStateOf(false) }
     val loginState by loginWithGoogleViewModel.loginState.collectAsState()
     val user by loginWithGoogleViewModel.user.collectAsState()
 
@@ -61,25 +68,23 @@ fun LoginForm(
                     onGoogleLogin(it)
                 }
                 Log.e("User photo", user?.photoUrl.toString())
-            }
+        }
             is LoginWithGoogleViewModel.LoginResult.Failure -> {
-
-                val errorMessage = (loginState as LoginWithGoogleViewModel.LoginResult.Failure).message
+                val errorMessage =
+                    (loginState as LoginWithGoogleViewModel.LoginResult.Failure).message
                 Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
             }
             else -> {}
+
         }
     }
 
-    Column {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        // Titulo
         Text(
-            text = "Sign in with",
-            color = Color.Black,
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Medium
-        )
-        Text(
-            text = "Email and Password",
+            text = "Sign in with password",
             color = Color.Black,
             fontSize = 30.sp,
             fontWeight = FontWeight.Medium
@@ -87,69 +92,111 @@ fun LoginForm(
 
         Spacer(modifier = Modifier.height(30.dp))
 
+        // Campo de email
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             value = email,
-            onValueChange = {email = it},
+            onValueChange = { email = it },
             label = { Text(text = "Email") },
             textStyle = TextStyle(color = Color.Black)
         )
 
         Spacer(modifier = Modifier.height(15.dp))
 
+        // Campo de senha
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             value = password,
-            onValueChange = {password = it},
+            onValueChange = { password = it },
             label = { Text(text = "Password") },
             textStyle = TextStyle(color = Color.Black)
         )
 
+        Spacer(modifier = Modifier.height(10.dp))
+
+        // caixa de seleção remember e reset password alinhados
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween, // deixa um elemento no inicio e outro no final
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = rememberMe,
+                    onCheckedChange = { rememberMe = it },
+                    colors = CheckboxDefaults.colors(
+                        checkedColor = Color.Blue.copy(alpha = 0.6f)
+                    )
+                )
+                Text(
+                    text = "Remember me",
+                    color = Color.Black,
+                    fontSize = 15.sp
+                )
+            }
+
+            Text(
+                text = "Reset password",
+                color = ForgotColor,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.clickable {
+                    // tela de modificar senha
+                }
+            )
+        }
+
         Spacer(modifier = Modifier.height(20.dp))
 
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = "Forgot Password?",
-            color = ForgotColor,
-            fontSize = 15.sp,
-            fontWeight = FontWeight.Medium,
-            textAlign = TextAlign.Right,
-        )
-
-        Spacer(modifier = Modifier.height(13.dp))
 
         MainButtonComponent(
-            text = "Sign In",
+            text = "SIGN IN",
             onClick = { onLogin(AuthModel(email, password)) },
-            color = Color.Blue.copy(alpha = 0.4f),
-            colorText = Color.White
+            color = BlueLogo,
+            colorText = Color.White,
         )
 
-        Spacer(modifier = Modifier.height(45.dp))
+        Spacer(modifier = Modifier.height(25.dp))
+
 
         TextDivider(text = "or continue with")
 
         Spacer(modifier = Modifier.height(25.dp))
 
-        Row(
-            modifier = Modifier.fillMaxWidth().background(Color.Blue.copy(alpha = 0.4f)),
-            horizontalArrangement = Arrangement.SpaceAround
-
-        ) {
-            IconButtonComponent(icon = R.drawable.google_logo, onClick = {
-                Log.d("LoginScreen", "Google button clicked")
-                loginWithGoogleViewModel.loginWithGoogle(context)
-            })
-        }
-
-        Spacer(modifier = Modifier.height(65.dp))
-
+        // opcoes de login
         Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
+            horizontalArrangement = Arrangement.SpaceEvenly // espacos iguais entre elementos
+        ) {
+            IconButtonComponent(icon = R.drawable.facebook_logo, onClick = {
+                //logarr com face
+            })
+            IconButtonComponent(icon = R.drawable.google_logo, onClick = {
+                loginWithGoogleViewModel.loginWithGoogle(context)
+            })
+            IconButtonComponent(icon = R.drawable.apple_logo, onClick = {
+               //logar com a apple
+           })
+        }
 
-            ) {
-            Text(text = "Don´t have an account? Sign up", color = Color.Black)
+        Spacer(modifier = Modifier.height(30.dp))
+
+        //cadastro
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Don’t have an account? ", color = Color.Black)
+            Text(
+                text = "Sign up",
+                color = ForgotColor,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.clickable {
+                    // tela de cadastro
+                }
+            )
         }
     }
 }
