9786e3d583babd1bdb86396aaf9a413f1da1b53f
Felipe silva <lipesx07@gmail.com>
2025-08-19 17:32:18 -0300

    adicionando tela de create password

---
+++ app/src/main/java/com/example/tradewolfapp/views/auth/Login/CreatePassword.kt
@@ -0,0 +1,132 @@
+package com.example.tradewolfapp.views.auth.Login
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.tradewolfapp.ui.theme.BlueLogo
+import com.example.tradewolfapp.views.components.MainButtonComponent
+
+@Composable
+fun CreatePassword(navController : NavController) {
+    var password by remember { mutableStateOf("") }
+    var repeatPassword by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
+    var repeatPasswordVisible by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(22.dp)
+            .background(Color.White),
+        verticalArrangement = Arrangement.spacedBy(24.dp)
+    ) {
+
+        IconButton(
+            onClick = { navController.popBackStack() },
+            modifier = Modifier.align(Alignment.Start)
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = "Voltar",
+                modifier = Modifier.size(24.dp)
+            )
+        }
+
+        Text(
+            text = "Create password",
+            color = Color.Black,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Medium
+        )
+
+        Text(
+            text = "Create your new password to login",
+            fontSize = 15.sp,
+            color = Color.Gray
+        )
+
+
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp),
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "Password") },
+            textStyle = TextStyle(color = Color.Black),
+            shape = androidx.compose.material3.MaterialTheme.shapes.medium,
+            singleLine = true,
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                val image =
+                    if (passwordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                    Icon(imageVector = image, contentDescription = "Toggle password visibility")
+                }
+            }
+        )
+
+
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp),
+            value = repeatPassword,
+            onValueChange = { repeatPassword = it },
+            label = { Text(text = "Repeat Password") },
+            textStyle = TextStyle(color = Color.Black),
+            shape = androidx.compose.material3.MaterialTheme.shapes.medium,
+            singleLine = true,
+            visualTransformation = if (repeatPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                val image =
+                    if (repeatPasswordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                IconButton(onClick = { repeatPasswordVisible = !repeatPasswordVisible }) {
+                    Icon(
+                        imageVector = image,
+                        contentDescription = "Toggle repeat password visibility"
+                    )
+                }
+            }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        MainButtonComponent(
+            text = "Submit",
+            onClick = { },
+            color = BlueLogo,
+            colorText = Color.White
+        )
+    }
+}
+
