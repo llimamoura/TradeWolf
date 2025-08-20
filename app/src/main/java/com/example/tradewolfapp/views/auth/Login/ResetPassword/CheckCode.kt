e5edf97ed738e4b1f114c1c135bb2bcccbe28fd9
Felipe silva <lipesx07@gmail.com>
2025-08-20 16:14:55 -0300

    add icon de retornar para tela anterior

--- app/src/main/java/com/example/tradewolfapp/views/auth/Login/ResetPassword/CheckCode.kt
+++ app/src/main/java/com/example/tradewolfapp/views/auth/Login/ResetPassword/CheckCode.kt
@@ -9,12 +9,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -24,12 +30,13 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.example.tradewolfapp.ui.theme.BlueLogo
 import com.example.tradewolfapp.ui.theme.ForgotColor
 import com.example.tradewolfapp.views.components.MainButtonComponent
 
 @Composable
-fun CheckCode() {
+fun CheckCode(navController: NavController) {
     val otpLength = 4
     var otpValues = remember { mutableStateOf(List(otpLength) { "" }) }
     var focusRequesters = List(otpLength) { FocusRequester() }
@@ -42,6 +49,18 @@ fun CheckCode() {
             .background(Color.White),
         verticalArrangement = Arrangement.spacedBy(24.dp)
     ) {
+        IconButton(
+            onClick = { navController.popBackStack() },
+            modifier = Modifier.align(Alignment.Start)
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = "Voltar",
+                modifier = Modifier.size(24.dp)
+            )
+        }
+
+
         Text(
             text = "Enter verification Code",
             color = Color.Black,
