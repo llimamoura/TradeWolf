2648de9295a8543fd680ebf6286fedaaa45b98b2
Felipe silva <lipesx07@gmail.com>
2025-08-21 23:24:56 -0300

    modifying the mainbuttoncomponent

--- app/src/main/java/com/example/tradewolfapp/views/components/MainButtonComponent.kt
+++ app/src/main/java/com/example/tradewolfapp/views/components/MainButtonComponent.kt
@@ -1,24 +1,19 @@
 package com.example.tradewolfapp.views.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
@@ -26,8 +21,9 @@ import androidx.compose.ui.unit.dp
 fun MainButtonComponent(
     text: String,
     onClick: () -> Unit,
-    color: Color,
-    colorText: Color,
+    colorStart: Color, 
+    colorEnd: Color,   
+    colorText: Color   
 ) {
     Card(
         modifier = Modifier
@@ -36,17 +32,21 @@ fun MainButtonComponent(
         onClick = onClick,
         shape = RoundedCornerShape(10.dp),
         colors = CardDefaults.cardColors(
-            containerColor = color
+            containerColor = Color.Transparent 
         ),
-
-        ) {
+    ) {
         Row(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(colorStart, colorEnd)
+                    )
+                ),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
             Text(text = text, color = colorText)
-
         }
     }
 }
