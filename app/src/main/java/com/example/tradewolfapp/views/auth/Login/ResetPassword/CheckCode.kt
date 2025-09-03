c6d027e49a9e3ae93c6bb77f45465c6d6fc6fc66
Felipe silva <lipesx07@gmail.com>
2025-09-03 14:32:12 -0300

    style: improving the interface in IdCard and removing Color in CheckCode

--- app/src/main/java/com/example/tradewolfapp/views/auth/Login/ResetPassword/CheckCode.kt
+++ app/src/main/java/com/example/tradewolfapp/views/auth/Login/ResetPassword/CheckCode.kt
@@ -20,6 +20,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.*
 import androidx.navigation.NavController
 import com.example.tradewolfapp.ui.theme.BlueLogo
+import com.example.tradewolfapp.ui.theme.DeepBlue
+import com.example.tradewolfapp.ui.theme.CobaltBlue
 import com.example.tradewolfapp.ui.theme.ForgotColor
 import com.example.tradewolfapp.views.components.MainButtonComponent
 
@@ -129,8 +131,9 @@ fun CheckCode(navController: NavController) {
         MainButtonComponent(
             text = "Submit",
             onClick = { navController.navigate("createpassword") },
-            color = BlueLogo,
-            colorText = Color.White
+            colorText = Color.White,
+            colorStart = DeepBlue,
+            colorEnd = CobaltBlue
         )
     }
 }
